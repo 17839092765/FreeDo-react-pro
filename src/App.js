8df111869b57fb0b7185518cf51a6f3db8f5bca0
17839092765 <1341307180@qq.com>
2021-04-13 11:08:59 +0800

    frist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home";
 function App() {
+  const [api, setapi] = useState();
+
+  useEffect(() => {
+    const Wsurl = "127.0.0.1:4322";
+    // setapi(new AirCityAPI(Wsurl, onReady, log));
+  }, []);
+
+  const log = () => {};
+  const onReady = () => {};
+  const onEvent = (e) => {
+    console.log(e);
+  };
+  if (api) {
+    console.log(api);
+    api.setEventCallback(onEvent);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            {/* <Route path="/login" component={}></Route> */}
+            <Route path="/" component={Home}></Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+      <div id="player"></div>,
+    </>
   );
 }
 
